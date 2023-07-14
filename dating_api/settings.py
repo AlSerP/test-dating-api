dee5b9eb8b006d7fad7883fae462fe1008b43e47
George Pogrebnoi <gosha1416@yandex.ru>
2023-07-14 15:00:57 +0300

    Fix

--- dating_api/settings.py
+++ dating_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)u91+iul70i26p533p($#^klg!*l^)hlodp9dv5y0o&x_hbwf%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
